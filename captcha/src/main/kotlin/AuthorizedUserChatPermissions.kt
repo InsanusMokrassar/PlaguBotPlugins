d9f960e8152a388209395242098b8668ca82d1de
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2023-02-06 22:43:01 +0600

    fixes

--- captcha/src/main/kotlin/AuthorizedUserChatPermissions.kt
+++ captcha/src/main/kotlin/AuthorizedUserChatPermissions.kt
@@ -1,6 +1,7 @@
 package dev.inmo.plagubot.plugins.captcha
 
+import dev.inmo.tgbotapi.types.chat.LeftRestrictionsChatPermissions
 import dev.inmo.tgbotapi.types.chat.RestrictionsChatPermissions
 
-@Deprecated("Useless", ReplaceWith("RestrictionsChatPermissions", "dev.inmo.tgbotapi.types.chat.RestrictionsChatPermissions"))
-val authorizedUserChatPermissions = RestrictionsChatPermissions
+@Deprecated("Useless", ReplaceWith("LeftRestrictionsChatPermissions", "dev.inmo.tgbotapi.types.chat.LeftRestrictionsChatPermissions"))
+val authorizedUserChatPermissions = LeftRestrictionsChatPermissions
